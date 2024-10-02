04518a9fac2ef141d3e8c2ea1d362be1825c2378
miladdg <milad.d.ghane@live.com>
2024-10-02 09:37:49 +0200

    Arduino und BMC

--- ArduinoCode/FirstDraft.ino/FirstDraft.ino.ino
+++ ArduinoCode/FirstDraft.ino/FirstDraft.ino.ino
@@ -7,35 +7,38 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-const int rs = 2, en = 3, d4 = 6, d5 = 7, d6 = 8, d7 = 9;
-LiquidCrystal lcd (rs, en, d4, d5, d6, d7);
-
+const int rs = 10, en = 9, d4 = 7, d5 = 6, d6 = 5, d7 = 4;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 
 void setup() {
 
-Serial.begin(9600);
+// Serial.begin(9600);
 
 dht.begin();
 
 lcd.begin(16, 2);
-
 }
 
 void loop() {
-  delay(2000);
+
   lcd.clear();
-  lcd.setCursor(0,0);
+
   float humidity = dht.readHumidity();
   float temp = dht.readTemperature();
 
+  lcd.setCursor(0,0);
+  lcd.print("hi");
   lcd.print("Temp: ");
   lcd.print(temp);
   lcd.print(" C");
-
+  // Serial.print("Temp: ");
+  // Serial.print(temp);
+  // Serial.println();
+  
   lcd.setCursor(0,1);
   lcd.print("Hum.: ");
   lcd.print(humidity);
   lcd.print(" %");
-
+  delay(2000);
 }
